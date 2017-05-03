9518580ea58993d3f0c4e49268e84525e24d8493
yymartin <yoan.martin@epfl.ch>
2017-05-03 13:14:54 +0200

    new version of queries.sql

--- queries.sql
+++ queries.sql
@@ -15,3 +15,35 @@ WHERE H.serie_id = H.type_id and Serie_type.name = "magazine" and S.country_id =
 /*f) Print the titles of the 10 most reprinted stories */
 /*g) Print the artists that have scripted, drawn, and colored at least one of the stories they where involved in */
 /*h) Print all non-reprinted stories involving Batman as a non-featured character */
+
+b) Print the ids and names of publishers of Danish book series.
+Select P.id, P.name
+From Publisher P, Series S, Country C
+Where S.country_id = C.id and C.name = "Danemark" and S.publisher_id = P.id
+
+c) Print the names of all Swiss series that have been published in magazines.
+Select S.name
+From Series S, Country C, Serie_Type T, Has_Serie_Type H
+Where H.serie_id = and Serie_type.name = "magazine" and S.country_id = C.id and C.name = "Switzerland"
+
+d) Starting from 1990, print the number of issues published each year.
+Select *
+From Issue I
+Group By I.publication_date
+Where I.publication_date >= 1990
+
+e) Print the number of series for each indicia publisher whose name resembles ‘DC comics’.
+Select Count(distinct *)
+From Issue I, Series S
+Where (I.id = S.first_issue_id or I.id = S.last_issue_id) and I.Indiciapublisher_id IN(
+Select IP.id
+From IndiciaPublisher IP
+Where IP.name = "DC comics")
+
+f) Print the titles of the 10 most reprinted stories
+
+
+g) Print the artists that have scripted, drawn, and colored at least one of the stories they were involved in.
+
+
+h) Print all non-reprinted stories involving Batman as a non-featured character.
