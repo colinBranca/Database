2898fa30b97b0bc245123f0bf1b285bbae7bed60
yymartin <yoan.martin@epfl.ch>
2017-05-27 16:37:22 +0200

    Adding new java applet with the backend finished

---
+++ DataBaseInterface/DataBaseInterface/src/Main.java
@@ -0,0 +1,8 @@
+import Model.DatabaseConnector;
+
+public class Main {
+	public static void main(String args[]){
+		DatabaseConnector dbConnector = new DatabaseConnector();
+		dbConnector.searchElement();
+	}
+}
