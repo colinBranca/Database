445793917ee9f299671088020363f795a1c39090
yymartin <yoan.martin@epfl.ch>
2017-05-28 22:12:09 +0200

    The user interface is almost finished
    TODO : insert and delete

--- DataBaseInterface/DataBaseInterface/src/Main.java
+++
@@ -1,8 +0,0 @@
-import Model.DatabaseConnector;
-
-public class Main {
-	public static void main(String args[]){
-		DatabaseConnector dbConnector = new DatabaseConnector();
-		dbConnector.searchElement();
-	}
-}
